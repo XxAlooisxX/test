bdada88bb99b65f0d6e02c9aad63953f1eda5246
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-07-29 11:42:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
+	bot.user.setStatus("online")
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
