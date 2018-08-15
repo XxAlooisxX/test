b7117fc8c54b136ca35581289ea9c5fae0f5981b
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-08-15 14:34:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -890,7 +890,7 @@ client.on('message', message => {
 
 if (command == "embed") {
     let say = new Discord.RichEmbed()
-    .setAuthor(message.author)
+    .setAuthor(message)
     .setDescription(args.join("  "))
     .setColor(0x00AE86)
     message.channel.sendEmbed(say);
